2e2f192313092cb2ef397cd7b3ef9b245560f042
SidhantSinghGuleria <sidhantsingh1997@gmail.com>
2025-06-08 14:25:00 +0530

    Addign Data

---
+++ VendingMachine/Data/ProductData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using VendingMachineApp.Models;
+
+namespace VendingMachineApp.Data
+{
+    public static class ProductData
+    {
+        public static readonly Dictionary<string, Product> ProductPriceList = new Dictionary<string, Product>(System.StringComparer.OrdinalIgnoreCase)
+        {
+            { "cola", new Product("cola", 1.00m) },
+            { "chips", new Product("chips", 0.50m) },
+            { "candy", new Product("candy", 0.65m) }
+        };
+    }
+}
